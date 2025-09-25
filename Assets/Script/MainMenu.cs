45b0ae5dd4b127aac696eea8a55467a34fe0fc20
Chanawit Jindatanasit <gget.2547@gmail.com>
2025-09-26 01:55:48 +0700

    MainMenu page
    create MainMenu and link to home and setting

---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void NewGame ()
+    {
+        SceneManager.LoadScene("Home");
+    }
+    public void ContinuosGame()
+    {
+        SceneManager.LoadScene("Home");
+    }
+    public void QuitGame ()
+    {
+        Debug.Log("Quit GAme");
+        Application.Quit();
+    }
+}
